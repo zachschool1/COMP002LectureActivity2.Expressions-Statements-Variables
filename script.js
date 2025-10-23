8646b348b939717cc22281c2b031dd12c1c97251
Zachary Jordan <zacharyjordan@my.whccd.edu>
2025-10-22 20:14:03 -0700

    delete the stuff from previous commit so I can turn it in as it was asked.

--- script.js
+++ script.js
@@ -5,10 +5,8 @@ console.log(myGreeting);
 //Define a constant variable “pi” and place a number containing the value for Pi inside of it.
 const pi = 3.14;
 //Attempt to re-assign “pi” to something else - what happens? Why?
-//pi = 2; //error, can't change pi as it is a const
+pi = 2; //error, can't change pi as it is a const
 //Define a new variable “myNumber” and place a number of your choice inside it.
 let myNumber = 21;
 //Call the “min” function inside of the “Math” object on both “myNumber” and “pi” - what is the result? Why?
-let minNum = Math.min(pi,myNumber); //result would be 3.14, pi since it is lower than what i set myNumber to, 21.
-
-console.log(minNum);
+Math.min(pi,myNumber); //result would be 3.14, pi since it is lower than what i set myNumber to, 21.

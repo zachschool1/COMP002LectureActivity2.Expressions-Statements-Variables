ae55cde31d2b670feb7c21a741a23e99db02bea3
Zachary Jordan <zacharyjordan@my.whccd.edu>
2025-10-22 20:12:17 -0700

    first commit

---
+++ readme.md
@@ -0,0 +1,2 @@
+purpose: Week 2 Lecture Activity #2: Expressions, Statements, and Variables
+author: Zachary Jordan
---
+++ script.js
@@ -0,0 +1,12 @@
+//Define a variable “myGreeting” and place a string containing a generic greeting inside it.
+let myGreeting = "This is my greeting!";
+//Output the contents of “myGreeting” using console.log(...).
+console.log(myGreeting);
+//Define a constant variable “pi” and place a number containing the value for Pi inside of it.
+const pi = 3.14;
+//Attempt to re-assign “pi” to something else - what happens? Why?
+//pi = 2; //error, can't change pi as it is a const
+//Define a new variable “myNumber” and place a number of your choice inside it.
+let myNumber = 21;
+//Call the “min” function inside of the “Math” object on both “myNumber” and “pi” - what is the result? Why?
+minNum = Math.min(pi,myNumber); //result would be 3.14, pi since it is lower than what i set myNumber to, 21.
